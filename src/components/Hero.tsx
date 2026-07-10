75d9a1ad8b5dc542099902acaf6dc089d787282e
Dwayne1828 <dwaynebarrameda093@gmail.com>
2026-07-11 05:35:53 +0800

    Fixed Deployment Error

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -1,5 +1,5 @@
 import { motion } from 'framer-motion';
-import { ArrowRight, Briefcase, GraduationCap, User } from 'lucide-react';
+import { ArrowRight, User } from 'lucide-react';
 export function Hero() {
   return (
     <section
